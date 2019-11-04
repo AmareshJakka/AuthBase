7eb02d48722faf6e26775d898904c40876dc24e7
AmareshJakka <amareswararaojakka@gmail.com>
2019-11-05 00:53:46 +0530

    Added navigation and Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [{path:  "", pathMatch:  "full",redirectTo:  "home"},
+{path: "home", component: HomeComponent},
+{path: "employee-list", component: EmployeeListComponent},
+{path: "add-employee", component: AddEmployeeComponent} ,
+{path:"contactUs" ,component:ContactUsComponent} ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { HomeComponent } from './home/home.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import{HeaderComponent} from './header/header.component';
+import{FooterComponent} from './footer/footer.component';
+
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    HomeComponent,
+    EmployeeListComponent,
+    AddEmployeeComponent,
+    ContactUsComponent ,
+    HeaderComponent,
+    FooterComponent ],
   imports: [
     BrowserModule,
     AppRoutingModule
